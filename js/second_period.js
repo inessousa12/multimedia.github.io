37ffb150f255628a0185f6aabaab3c1a2dbc83e4
Ines Sousa <fc51588@alunos.fc.ul.pt>
2021-05-26 20:30:42 +0100

    change second_period to youtube

--- js/second_period.js
+++ js/second_period.js
@@ -1,13 +1,11 @@
 $(document).ready(function($){
 
-    
     $("img").on("click", function() {
-        var video = "https://player.vimeo.com/video/555266483?autoplay=1#"
+        var video = "https://www.youtube.com/embed/71Ln5CXWmoU?autoplay=1&"
         var min = $(this).attr("id");
         console.log(min)
-        video += "t=" + min;
-        $(".video_indexing iframe").attr("src", video);
+        video += "start=" + min + "&autoplay=1";
         console.log(video);
-        // document.getElementsByTagName('iframe')[0].src = video;
+        document.getElementsByTagName('iframe')[0].src = video;
     });
-})
+}) 
