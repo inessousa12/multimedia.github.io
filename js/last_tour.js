1029d591e9607d01c0b3e65cb9d175afb575ae8b
David Silva <davidmssilva5@gmail.com>
2021-05-29 15:51:22 +0100

    add id ao iframe

--- js/last_tour.js
+++ js/last_tour.js
@@ -3,14 +3,12 @@ $(document).ready(function($){
     $("#map-europe").CSSMap({
         "size": 540,
         "onClick": function(e){
-        // id do elemento = segundos do video que temos de enviar
-        // console.log(e[0].id);
-        var video = "https://www.youtube.com/embed/JkbTI-UPdZs?autoplay=1&"
-        var min = e[0].id;
-        console.log(min)
-        video += "start=" + min + "&autoplay=1";
-        console.log(video);
-        document.getElementsByTagName('iframe')[0].src = video;
+            var video = "https://www.youtube.com/embed/JkbTI-UPdZs?autoplay=1&"
+            var min = e[0].id;
+            console.log(min)
+            video += "start=" + min + "&autoplay=1";
+            console.log(video);
+            document.getElementsByTagName('iframe')[0].src = video;
         },
     });
 });
