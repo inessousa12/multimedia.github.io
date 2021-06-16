b82cdf1d25a872e32655ff78a90f7a87575ef6c0
Diogo Frazao <doga.frazao@gmail.com>
2021-06-16 17:31:50 +0100

    First Period files

---
+++ js/first_period.js
@@ -0,0 +1,11 @@
+$(document).ready(function($){
+
+    $("img").on("click", function() {
+        var video = "https://www.youtube.com/embed/DcqxuSFbsOI?autoplay=1&"
+        var min = $(this).attr("id");
+        console.log(min)
+        video += "start=" + min + "&autoplay=1";
+        console.log(video);
+        document.getElementsByTagName('iframe')[0].src = video;
+    });
+}) 
