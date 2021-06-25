7be3f26c16d44804a105abb053210c313771d708
xicoramos29 <franciscoramos98@hotmail.com>
2021-06-25 14:45:20 +0100

    alterações selecionar país

--- js/last_tour.js
+++ js/last_tour.js
@@ -4,12 +4,24 @@ $(document).ready(function($){
     $("#map-europe").CSSMap({
         "size": 540,
         "onClick": function(e){
-            var video = "https://www.youtube.com/embed/JkbTI-UPdZs?autoplay=1&"
             var min = e[0].id;
-            console.log(min)
-            video += "start=" + min + "&autoplay=1";
-            console.log(video);
-            document.getElementsByTagName('iframe')[0].src = video;
+            player.loadVideoById("JkbTI-UPdZs", min, "default");
         },
     });
 });
+
+// $(document).ready(function($){
+
+//     // CSSMap;
+//     $("#map-europe").CSSMap({
+//         "size": 540,
+//         "onClick": function(e){
+//             var video = "https://www.youtube.com/embed/JkbTI-UPdZs?autoplay=1&"
+//             var min = e[0].id;
+//             console.log(min)
+//             video += "start=" + min + "&autoplay=1";
+//             console.log(video);
+//             document.getElementsByTagName('iframe')[0].src = video;
+//         },
+//     });
+// });
